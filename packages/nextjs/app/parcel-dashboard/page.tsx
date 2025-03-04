01390e06785d4c396905f46a23fea8ae87addb85
WenHao1223 <wenhaojshs@gmail.com>
2025-03-04 21:28:48 +0800

    Add action buttons

--- packages/nextjs/app/parcel-dashboard/page.tsx
+++ packages/nextjs/app/parcel-dashboard/page.tsx
@@ -59,6 +59,33 @@ const ParcelDashboard: NextPage = () => {
                 <th>
                   <div className="badge badge-soft badge-info badge-sm">Sent</div>
                 </th>
+                <th>
+                  <div className="list-row">
+                    <button className="btn btn-square btn-ghost">
+                      <div className="tooltip" data-tip="Track delivery">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                          />
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
+                </th>
               </tr>
               {/* row 2 */}
               <tr>
@@ -71,6 +98,51 @@ const ParcelDashboard: NextPage = () => {
                 <td>234, Main Street, 1234 New York, United States</td>
                 <th>
                   <div className="badge badge-soft badge-warning badge-sm">Pending</div>
+                </th>
+                <th>
+                  <div className="list-row">
+                    <button className="btn btn-square btn-ghost">
+                      <div className="tooltip" data-tip="Track delivery">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                          />
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                    <button className="btn btn-square btn-ghost">
+                      <div className="tooltip" data-tip="Send product">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
                 </th>
               </tr>
               {/* row 3 */}
@@ -85,6 +157,33 @@ const ParcelDashboard: NextPage = () => {
                 <th>
                   <div className="badge badge-soft badge-success badge-sm">Arrived</div>
                 </th>
+                <th>
+                  <div className="list-row">
+                    <button className="btn btn-square btn-ghost">
+                      <div className="tooltip" data-tip="Track delivery">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                          />
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
+                </th>
               </tr>
               {/* row 4 */}
               <tr>
@@ -97,6 +196,33 @@ const ParcelDashboard: NextPage = () => {
                 <td>456, Main Street, 1234 New York, United States</td>
                 <th>
                   <div className="badge badge-soft badge-error badge-sm">Cancelled</div>
+                </th>
+                <th>
+                  <div className="list-row">
+                    <button className="btn btn-square btn-ghost">
+                      <div className="tooltip" data-tip="Track delivery">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                          />
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </div>
                 </th>
               </tr>
             </tbody>
