44555bbe10aa737f6cc856da5e709eb8f417e0e7
WenHao1223 <wenhaojshs@gmail.com>
2025-03-05 19:48:02 +0800

    Update data structure

--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import parcelJSON from "../../public/data/parcel.json";
 import { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
+  const [tableData, setTableData] = useState(null);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Dashboard</h1>
@@ -48,48 +54,6 @@ const Dashboard: NextPage = () => {
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
-              <tr>
-                <td>123456789</td>
-                <td className="link hover:text-secondary">Doe</td>
-                <td className="link hover:text-secondary">John</td>
-                <td>
-                  Parcel ABC <br />
-                  <span className="badge badge-outline badge-warning badge-sm">Regional</span>
-                </td>
-                <td>123, Main Street, 1234 New York, United States</td>
-                <td>
-                  <div className="badge badge-soft badge-info badge-sm">Sent</div>
-                </td>
-                <td>
-                  <div className="list-row">
-                    <button className="btn btn-square btn-ghost">
-                      <div className="tooltip" data-tip="Track delivery">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                          />
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                          />
-                        </svg>
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              {/* row 2 */}
               <tr>
                 <td>234567890</td>
                 <td className="link hover:text-secondary">Bob</td>
@@ -141,88 +105,6 @@ const Dashboard: NextPage = () => {
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                          />
-                        </svg>
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <td>987654321</td>
-                <td className="link hover:text-secondary">Jet</td>
-                <td className="link hover:text-secondary">Charles</td>
-                <td>
-                  Parcel ABC <br />
-                  <span className="badge badge-outline badge-warning badge-sm">Regional</span>
-                </td>
-                <td>345, Main Street, 1234 Bristol, United Kingdom</td>
-                <td>
-                  <div className="badge badge-soft badge-success badge-sm">Arrived</div>
-                </td>
-                <td>
-                  <div className="list-row">
-                    <button className="btn btn-square btn-ghost">
-                      <div className="tooltip" data-tip="Track delivery">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                          />
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                          />
-                        </svg>
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              {/* row 4 */}
-              <tr>
-                <td>098765432</td>
-                <td className="link hover:text-secondary">Dane</td>
-                <td className="link hover:text-secondary">John</td>
-                <td>
-                  Parcel ABC <br />
-                  <span className="badge badge-outline badge-success badge-sm">International</span>
-                </td>
-                <td>456, Main Street, 1234 New York, United States</td>
-                <td>
-                  <div className="badge badge-soft badge-error badge-sm">Cancelled</div>
-                </td>
-                <td>
-                  <div className="list-row">
-                    <button className="btn btn-square btn-ghost">
-                      <div className="tooltip" data-tip="Track delivery">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                          />
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
                           />
                         </svg>
                       </div>
