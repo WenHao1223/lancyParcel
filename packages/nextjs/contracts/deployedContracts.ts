2db3136ca7ba9714f47b67a752c7b3387c3cf48c
mingjia <lmj11083@gmail.com>
2025-03-06 19:07:50 +0800

    working halfway on sending email, to be continued after integrating create

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     YourContract: {
-      address: "0x700b6a60ce7eaaea56f065753d8dcb9653dbad35",
+      address: "0xa15bb66138824a1c7167f5e85b957d04dd34e468",
       abi: [
         {
           type: "constructor",
@@ -579,7 +579,7 @@ const deployedContracts = {
         },
       ],
       inheritedFunctions: {},
-      deploymentFile: "run-1741198930.json",
+      deploymentFile: "run-1741245717.json",
       deploymentScript: "Deploy.s.sol",
     },
   },
