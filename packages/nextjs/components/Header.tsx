c103640f4f31e5695c04e28569ef4bcd4ac83ed8
WenHao1223 <wenhaojshs@gmail.com>
2025-03-08 08:34:41 +0800

    Update header and footer

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -21,11 +21,11 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
 
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
+  // {
+  //   label: "Debug Contracts",
+  //   href: "/debug",
+  //   icon: <BugAntIcon className="h-4 w-4" />,
+  // },
 ];
 
 export const HeaderMenuLinks = () => {
@@ -98,8 +98,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo-black.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Lancy Parcel</span>
+            <span className="text-xs">Secure, Transparent, Immutable</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
